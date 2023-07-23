aab5b1781049444e3dae20e58b029ddfb7c1a861
Lucas Reis <lucasreis_9@hotmail.com>
2023-07-23 15:22:03 -0300

    Project Setup

---
+++ lib/constants.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+const kPrimaryColor = Color(0xFFFF7643);
+const kPrimaryLightColor = Color(0xFFFFECDF);
+const kPrimaryGradientCOlor = LinearGradient(
+    begin: Alignment.topLeft,
+    end: Alignment.bottomRight,
+    colors: [
+      Color(0XFF757575),
+      Color(0xFFFF7643),
+    ]);
+
+const kSeconderyColor = Color(0xFF979797);
+const kTextColor = Color(0xFF757575);
+
+const kAnimationDuration = Duration(milliseconds: 200);
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:ecommerce1/constants.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -11,13 +12,19 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-        
+        scaffoldBackgroundColor: Colors.white,
+        fontFamily: 'Muli',
+        textTheme: TextTheme(
+          bodyLarge: TextStyle(color: kTextColor),
+          bodyMedium: TextStyle(color: kTextColor),
+        ),
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: ,
+      home: Text('Home'),
     );
   }
 }
